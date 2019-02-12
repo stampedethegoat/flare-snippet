4b30bb9e60ddefa7919bd7ae99257f9d725ca92f
stampedethegoat <ben.basuni@datadoghq.com>
2019-02-11 19:38:03 -0800

    without function

--- unzipper.sh
+++ unzipper.sh
@@ -1,28 +1,24 @@
-flare ()
-{
-  # Set Variables
-  DATE=`date +'%m-%d'`
-  FILENAME=$2-${DATE}
+# Set Variables
+DATE=`date +'%m-%d'`
+FILENAME=$2-${DATE}
 
-  if [[ $1 =~ \.zip$ ]]; then
+if [[ $1 =~ \.zip$ ]]; then
 
-    # Unzip the File into ~/.flare/
-    unzip $1 -d ~/.flare/;
+  # Unzip the File into ~/.flare/
+  unzip $1 -d ~/.flare/;
 
-    # Remove Zipped File
-    rm -rf $1;
+  # Remove Zipped File
+  rm -rf $1;
 
-    # Change Directory into ~/.flare and clear history
-    cd ~/.flare; c;
+  # Change Directory into ~/.flare and clear history
+  cd ~/.flare; c;
 
-    if [ -z "$2"]; then
-      FILENAME='template-flare'
-    fi
-
-   cd */; pwd; ls -A;
-    # vim status.log # -c 'vs logs/agent.log'
-  else
-    pwd; ls -A; echo "There are no flares to unzip.. Please try again"
+  if [ -z "$2"]; then
+    FILENAME='template-flare'
   fi
-}
 
+ cd */; pwd; ls -A;
+  # vim status.log # -c 'vs logs/agent.log'
+else
+  pwd; ls -A; echo "There are no flares to unzip.. Please try again"
+fi
